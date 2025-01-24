1a6af9a6041c0b421789bdaf82666d700d21f007
GavrilS <gavr1lll@abv.bg>
2025-01-24 23:37:46 +0200

    Added income/expenditure functionality to the monthly finance card

--- main_functionality/monthly_financing.py
+++ main_functionality/monthly_financing.py
@@ -1,5 +1,5 @@
 """
-This is a model for the income/spenditures for a person in a month.
+This is a model for the income/expenditures for a person in a month.
 """
 
 class FinanceCard:
@@ -8,7 +8,7 @@ class FinanceCard:
         self._year = year
         self._month = month
         self._income_sources = {}
-        self._spenditures = {}
+        self._expenditures = {}
 
     @property
     def year(self):
@@ -24,10 +24,6 @@ class FinanceCard:
     @property
     def month(self):
         return self._month
-    
-    @property
-    def income(self):
-        return self._income_sources
 
     @month.setter
     def month(self, value):
@@ -35,15 +31,45 @@ class FinanceCard:
             raise Exception('The Month needs to be specified.')
         
         self._month = value
+
+    @property
+    def income(self):
+        return self._income_sources
     
-    def add_income(source, value):
+    def add_income(self, source, value):
         if not self.income.get(source, None):
             self.income[source] = value
         else:
             self.income.get(source) += value
 
-    def remove_income(source, value):
+    def remove_income(self, source, value):
         if self.income.get(source, None):
             self.income.get(source) -= value
             if self.income.get(source) <= 0:
                 del self.income.get(source)
+
+    def show_incomes(self):
+        print(f"Income for {self.month} {self.year}")
+        for k, v in self.income.items():
+            print(f" {k}: {v}")
+
+    @property
+    def expenditures(self):
+        return self._expenditures
+
+    def add_expenditure(self, source, value):
+        if not self.expenditures.get(source, None):
+            self.expenditures[source] = value
+        else:
+            self.expenditures.get(source) += value
+    
+    def remove_expenditure(self, source, value):
+        if self.expenditures.get(source, None):
+            self.expenditures.get(source) -= value
+            if self.expenditures.get(source) <= 0:
+                del self.expenditures.get(source)
+    
+    def show_expenditures(self):
+        print(f"Expenditures for {self.month} {self.year}")
+        for k, v in self.expenditures.items():
+            print(f" {k}: {v}")
