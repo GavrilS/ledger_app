ee457d4275ea3602a7d8d0a81910b7f53bf5dab1
GavrilS <gavr1lll@abv.bg>
2025-02-08 18:34:41 +0200

    Added option to specify active user and retrieve user for the command line utility

--- console_runner.py
+++ console_runner.py
@@ -1,5 +1,6 @@
 from console_utility.finance_commands import FinanceCommands
 from console_utility.user_commands import UserCommands
+from console_utility.app_data import AppData
 from ledger_app.finances.ledger import Ledger
 from ledger_app.finances.monthly_financing import MonthlyReport
 from ledger_app.auth.user import User
@@ -7,7 +8,7 @@ from ledger_app.auth.user import User
 class Engine:
 
     def __init__(self):
-        pass
+        self.app = AppData()
 
-    def 
+    
     
--- console_utility/app_data.py
+++ console_utility/app_data.py
@@ -7,23 +7,38 @@ class AppData:
     def __init__(self):
         self.users = []
         self.ledgers = {}
+        self.active_user = None
+
+    def set_active_user(self, user):
+        if not user in self.users:
+            print('User is not present in the list of saved users... Execute command Add User first...')
+            return
+        
+        self.active_user = user
 
     def add_user(self, user):
         self.users.append(user)
     
-    def add_user_ledger(self, user_mail, ledger):
-        if self.ledgers.get(user_mail, None):
+    def add_user_ledger(self, user, ledger):
+        if self.ledgers.get(user.email, None):
             confirmation = input('The user already has a ledger. Do you want to delete it and create a new one?[Yes, no]: ')
             if confirmation != 'Yes':
                 return
-        self.ledgers[user_mail] = ledger
+        self.ledgers[user.email] = ledger
         print('New ledger added to user ', user_mail)
 
-    def remove_user(self, user_email):
-        for user in self.users:
-            if user.email == user_email:
-                self.users.remove(user)
-                print('User removed: ', user)
-                return
+    def remove_user(self, user):
+        if user in self.users:
+            self.users.remove(user)
+            print('User removed: ', user)
+            return
 
         print('User was not found!')
+
+    def get_user_by_mail(self, user_mail):
+        for user in self.users:
+            if user.email == user_mail:
+                return user
+        
+        print('The user was not found!')
+        return None
