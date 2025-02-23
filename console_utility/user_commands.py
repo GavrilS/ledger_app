98eb8ff5408337e25bc54e4de2e169f079da2809
GavrilS <gavr1lll@abv.bg>
2025-02-23 21:39:42 +0200

    Added command funcitonality to remove users

--- console_utility/user_commands.py
+++ console_utility/user_commands.py
@@ -1,18 +1,20 @@
 """
 Commands to handle user operations - creation, deletion.
 """
+import ledger_app.auth.user as user
 
 class UserCommands:
 
-    def __init__(self):
-        pass
+    def __init__(self, app_data):
+        self.app_data = app_data
 
     def create_user(self):
         print('To create a new user add "Name", "Email" and "Password" when prompted:')
         user_name = input('Enter the name of the user: ')
         email = input('Enter the email of the user: ')
         password = input('Enter the user password: ')
-        return {'name': user_name, 'email': email, 'password': password}
+        app_data.add_user(user.User(user_name, email, password))
 
     def delete_user(self):
-        pass
+        email = input('Enter the email of the user to be deleted: ')
+        app_data.remove_user(email)
