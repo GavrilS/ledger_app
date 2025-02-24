a603fd6ef7995e22a3681f37df50cec827c24a09
GavrilS <gavr1lll@abv.bg>
2025-02-24 20:45:03 +0200

    Added greeting and get_user_input methods to introduce basic instrucitons for new users and parse user input for exit commands

--- console_runner.py
+++ console_runner.py
@@ -1,4 +1,5 @@
-from console_utility.finance_commands import build_user_ledger, build_monthly_finance_report
+import sys
+from console_utility.finance_commands import FinCommands
 from console_utility.user_commands import UserCommands
 from console_utility.app_data import AppData
 from console_utility.commands import AVAILABLE_COMMANDS
@@ -7,14 +8,34 @@ from ledger_app.finances.monthly_financing import MonthlyReport
 from ledger_app.auth.user import User
 import console_utility.commands as cmds
 
+EXIT_CMDS = [
+    'exit',
+    'quit',
+    'leave',
+    'end'
+]
+
 class Engine:
 
     def __init__(self):
         self.app = AppData()
 
     def run(self):
+        self._greeting()
         while True:
-            pass
+            cmd = self._get_user_input()
+
+    def _get_user_input(self):
+        user_input = input('Enter your command: ')
+        if user_input.lower() in EXIT_CMDS:
+            sys.exit()
+        return user_input
+    
+    def _greeting(self):
+        message = 'You are using the LedgerApp command line utility. For a list of available commands '
+        message += 'run "help". To quit type "exit". Thank you for using the tool!'
+        print(message)
+        print('='*100)
 
 
 if __name__=='__main__':
