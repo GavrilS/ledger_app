ea1e2f4b9f712fe0566c24d3490c61ec6d247ea4
GavrilS <gavr1lll@abv.bg>
2025-03-16 22:41:33 +0200

    Added logic for signup and additional redirect for login routes

--- ledger_app/auth/auth_routes.py
+++ ledger_app/auth/auth_routes.py
@@ -9,12 +9,29 @@ def login():
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
-        pass #ToDo get user from db and open user ledgers page
-    
+        if not email or not password:
+            raise Exception('User email and password are required to login...')
+        #ToDo get user from db and open user ledgers page
+        return render_template('user_ledgers.html')
+
     return render_template('login.html')
 
-@auth.route('/signup')
+@auth.route('/signup', method=('GET', 'POST'))
 def signup():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+        if not name or not email or not password:
+            raise Exception('Please populate all fields...')
+        if password != confirm_password:
+            raise Exception('Password mismatch...')
+        
+        user = User(name=name, email=email, password=password)
+        #ToDo add logic to save user to the DB
+        return render_template('login.html')
+    
     return render_template('signup.html')
 
 @auth.route('/logout')
